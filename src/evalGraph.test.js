cee1497b34ba58b1ed771bf1de5a69968a86ea3e
Per Fredelius <per.fredelius@findwise.com>
2018-07-08 16:14:21 +0200

    cleanup

--- src/evalGraph.test.js
+++ src/evalGraph.test.js
@@ -4,16 +4,6 @@ import testGraph from './testGraph';
 
 describe('graph optimization and evaluation', () => {
   test.only('evalGraph', () => {
-    // expect(pullGraph({
-    //   nodes: [
-    //     { type: 'Component', verb: 'plus' },
-    //     { type: 'Var', value: 1 },
-    //   ],
-    //   links: [
-    //     { target: }
-    //   ]
-    // }), ).toBe();
-    console.log('evalGraph(testGraph)', evalGraph(testGraph));
     expect(evalGraph(testGraph)[0]).toBeCloseTo(3, 2);
   });
 });
