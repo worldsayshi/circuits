b537dac9d1aa34bcd8bff8156d3ab3265cfc80b8
Per Fredelius <per.fredelius@findwise.com>
2018-07-09 00:12:45 +0200

    Change structure of return value from optimizeGraph

--- src/calc/optimizeGraph.js
+++ src/calc/optimizeGraph.js
@@ -1,12 +1,68 @@
 
-import fmin from 'fmin';
-import njs from '../numericjs/numeric-1.2.6';
-import evalGraph from './evalGraph';
+import { nelderMead } from 'fmin';
+// import evalGraph from './evalGraph';
+
+// Turn nodes into values
+export function nounify(nodeIds, nodes, nouns, x) {
+  return nodeIds.map((nodeId, index) => {
+    if (!nodes[nodeId]) {
+      throw new Error(`Missing node: ${nodeId}`);
+    }
+    return nouns[nodes[nodeId].noun](nodes[nodeId], index, x);
+  });
+
+}
+
+function getInitialVariableValues(nodes) {
+  return nodes
+    .filter(({ value, constant = false }) => !constant)
+    .map(({ value = 100 }) => value);
+}
+
+
+export function evalGraph ({ graph: { nodes, components }, nouns, verbs }, x) {
+  return components.reduce((acc, {left, right, verb}) => {
+    const leftValues = nounify(left, nodes, nouns, x);
+    const rightValues = nounify(right, nodes, nouns, x);
+    return acc + verbs[verb](leftValues, rightValues);
+  }, 0);
+}
+
+function insertValues(graph, values) {
+  let input = [...values];
+  return {
+    ...graph,
+    nodes: graph.nodes.map(node => {
+      if(!node.constant) {
+        let [newValue, ...rest] = input;
+        input = rest;
+        return {
+          ...node,
+          value: newValue,
+        };
+      }
+      return node;
+    }),
+  };
+}
 
 export default function optimizeGraph (graphContext) {
-  const optimization = fmin.nelderMead((x) => {
+
+  const initialValues = getInitialVariableValues(graphContext.graph.nodes);
+
+  const optimization = nelderMead((x) => {
     const sum = evalGraph(graphContext, x);
     return Math.abs(sum);
-  }, [100]);
-  return optimization.x.map(v => Math.round(v * 100) / 100);
+  }, initialValues);
+
+  const values = optimization.x.map(v => Math.round(v * 100) / 100);
+
+  console.log('values', values);
+
+  const optimizedGraph = insertValues(graphContext.graph, values);
+
+  return {
+    ...graphContext,
+    graph: optimizedGraph,
+  };
 };
--- src/calc/optimizeGraph.test.js
+++ src/calc/optimizeGraph.test.js
@@ -1,9 +1,50 @@
 
-import optimizeGraph from './optimizeGraph';
+import optimizeGraph, { nounify, evalGraph } from './optimizeGraph';
 import testGraph from '../testGraph';
+import {getNounResolvers} from "../nouns";
+// import evalGraph from "./evalGraph";
+
+describe('nounify', () => {
+  it('should return constants', () => {
+    const values = nounify([0], [{ noun: 'default', value: 4 }], getNounResolvers(), []);
+
+    expect(values).toEqual([4]);
+  });
+
+  it('should return variable injections', () => {
+    const values = nounify([0], [{ noun: 'default', constant: false }], getNounResolvers(), [4]);
+    expect(values).toEqual([4]);
+
+    const values2 = nounify([0], [{ noun: 'default' }], getNounResolvers(), [4]);
+    expect(values2).toEqual([4]);
+  });
+
+  it('more stuff', () => {
+    const values = nounify([2], [
+        { noun: 'default', constant: true, value: 1 },
+        { noun: 'default', constant: true, value: 2 },
+        { noun: 'default' },
+    ], getNounResolvers(), [4]);
+
+    expect(values).toEqual([4]);
+  })
+});
+
+describe('sum ops', () => {
+  test('evalGraph', () => {
+    expect(testGraph.verbs.sum([5,6], [11])).toBe(0);
+    expect(testGraph.verbs.sum([11], [6,5])).toBe(0);
+    expect(testGraph.verbs.sum([11, 69], [80])).toBe(0);
+    expect(testGraph.verbs.sum([-20], [-30,10])).toBe(0);
+    expect(evalGraph(testGraph, [4])).toBe(-1);
+    expect(evalGraph(testGraph, [3])).toBe(0);
+    expect(evalGraph(testGraph, [2])).toBe(1);
+    expect(evalGraph(testGraph, [1])).toBe(2);
+  });
+});
 
 describe('graph optimization and evaluation', () => {
   test('optimizeGraph', () => {
-    expect(optimizeGraph(testGraph).nodes[2].value).toBeCloseTo(3, 2);
+    expect(optimizeGraph(testGraph).graph.nodes[2].value).toBeCloseTo(3, 2);
   });
 });
