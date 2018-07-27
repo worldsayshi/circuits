87fb43e41a56ac39c70950358e02100f4153dd83
Per Fredelius <per.fredelius@findwise.com>
2018-07-27 17:49:55 +0200

    Visual mark on constant nodes

--- src/app/node/Var.tsx
+++ src/app/node/Var.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 
 export default class Var extends React.Component<{ [key: string]: any }> {
   render() {
-    const {label, nodeRadius, color, group, x, y, onClick, dragStart, dragStop, ...rest} = this.props;
+    const {label, nodeRadius, color, group, x, y, onClick, dragStart, dragStop, constant, ...rest} = this.props;
+
+    const strokeOptions = constant ? { stroke: 'black', strokeWidth: 4 } : {};
     // console.log('rest', rest);
     return <g
       transform={`translate(${x},${y})`}
@@ -14,7 +16,10 @@ export default class Var extends React.Component<{ [key: string]: any }> {
       <circle
         r={nodeRadius}
 
-        style={{fill: color(group)}}
+        style={{
+          ...strokeOptions,
+          fill: color(group)
+        }}
       />
       <text className='node-label'>{label}</text>
     </g>;
