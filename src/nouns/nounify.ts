975e786cc66ceabd1bbaac03174e456df563fe8d
Per Fredelius <per.fredelius@findwise.com>
2018-08-02 22:15:24 +0200

    cleanify

--- src/nouns/nounify.ts
+++ src/nouns/nounify.ts
@@ -7,18 +7,3 @@ export default function nounify(nodes, nouns) {
     return nouns[node.noun](node);
   });
 }
-
-// Turn nodes into values
-// export default function nounify(nodeIds, nodes, nouns, x) {
-//   return nodeIds.map((nodeId, index) => {
-//     if (typeof nodeId === 'object') {
-//       throw new Error('nodeId should not be an object');
-//     }
-//     if (!nodes[nodeId]) {
-//       throw new Error(`Missing node: ${nodeId}`);
-//     }
-//     const node = nodes[nodeId];
-//     return nouns[node.noun](node, index, x);
-//   });
-//
-// }
