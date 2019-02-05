456b334e9bdf319bdf1c51b1ffa603e3b64fd404
Per Fredelius <per.fredelius@findwise.com>
2019-02-05 21:19:03 +0100

    Fix for empty components not being supported

--- src/model/components/sum/index.ts
+++ src/model/components/sum/index.ts
@@ -1,8 +1,13 @@
 
 const partialSum : (a : number | string, b : number | string) => string = (a, b) => `${a} + ${b}`;
 
+function zeroIfEmpty(l) {
+  if(!l.length) return ['0'];
+  else return l;
+}
+
 export default {
   img: 'img/thing.svg',
   operation: (left : (number | string)[], right : (number | string)[]) =>
-    `${left.reduce(partialSum)} - (${right.reduce(partialSum)})`
+    `${zeroIfEmpty(left).reduce(partialSum)} - (${zeroIfEmpty(right).reduce(partialSum)})`
 }
